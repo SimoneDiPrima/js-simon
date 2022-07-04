765cf49de736b617fcbd419578e559b4640871fd
SimoneDiPrima <simonediprimagenova@gmail.com>
2022-07-04 14:49:28 +0200

    generazione numeri casuali da 1 a 100  con un ciclo for

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-console.log(`js ok`);
+
+
+const gameNumber = document.getElementById(`game-number`);
+
+let randomNumber = 5;
+
+let varNum = ``;
+for(let i=0;i<randomNumber;i++){
+    const getRandomnumber = Math.floor(Math.random()*100 -1);
+    console.log(getRandomnumber);
+    varNum += ` ` + getRandomnumber;
+    gameNumber.innerText = varNum;
+}
